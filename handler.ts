ab5ea2c060d05198f0d369f6363fb92041a35281
Björn Tegelund <bjorn@tumblbug.com>
2017-08-18 15:43:18 +0900

    Remove console.logs from handler

--- handler.ts
+++ handler.ts
@@ -5,30 +5,22 @@ import * as cache from './src/cache'
 const debug = makeDebug('searchkr:handler')
 
 const performSearch = async (query: string): Promise<string> => {
-  console.log('a')
   const cacheResult = await cache.get(query)
 
-  console.log('d')
   if (cacheResult == null) {
     debug(`Cache miss, getting results for ${query}...`)
-    console.log('e')
     const searchResult = await searchKr(query)
-    console.log('f')
     debug(`Queried results for ${query}`)
     const stringifiedResult = JSON.stringify(searchResult)
     try {
-      console.log('g')
       await cache.set(query, stringifiedResult)
       debug(`Cached results for ${query}`)
-      console.log('h')
     } catch (error) {
-      console.log('j')
       debug('Could not set cache', query, stringifiedResult, error)
     }
     debug(`Returnign results for ${query}`)
     return stringifiedResult
   } else {
-    console.log('k')
     debug(`Cache hit for ${query}`)
     debug(`Returning results for ${query}`)
     return cacheResult

96e4cd9a4c07a38f00396f8991a2dd2264f7558e
Björn Tegelund <bjorn@tumblbug.com>
2017-11-14 17:50:39 +0900

    Add telegram bot

--- handler.ts
+++ handler.ts
@@ -1,4 +1,5 @@
 import { search } from './src/cached-search'
+import { handleUpdate } from './src/telegram-bot'
 
 export const seonbi: AWSLambda.ProxyHandler = (event, _context, callback) => {
   if (callback == null) {
@@ -18,4 +19,24 @@ export const seonbi: AWSLambda.ProxyHandler = (event, _context, callback) => {
     .catch(error => {
       callback(error, { statusCode: 500, body: '' })
     })
+}
+
+export const telegramBot: AWSLambda.ProxyHandler = (event, _context, callback) => {
+  if (!callback) {
+    return
+  }
+  if (!event.body || event.body == null) {
+    return callback(undefined, { statusCode: 400, body: '' })
+  }
+
+  try {
+    const update = JSON.parse(event.body)
+    handleUpdate(update)
+    callback(undefined, {
+      statusCode: 200,
+      body: '',
+    })
+  } catch (error) {
+    callback(error, { statusCode: 500, body: '' })
+  }
 }
---
+++ src/telegram-bot.ts
@@ -0,0 +1,33 @@
+import search, { Result, Meaning } from '@bjrnt/seonbi-core'
+const Telegraf: any = require('telegraf')
+
+const bot = new Telegraf('465288108:AAEy_fltPZdZQMsBaHX4zFEyJlxznCfknYY')
+
+function formatMeaning(meaning: Meaning, index: number, meanings: Meaning[]) {
+  return `
+    ${meanings.length > 1 ? `${index + 1}. ` : ''}*${meaning.translation}*
+    ${meaning.kr}
+    _${meaning.en}_`
+}
+
+function formatResult(result: Result, index: number, results: Result[]): string {
+  return `
+${results.length > 1 ? `${index + 1}.` : ''} *${result.word}* ${result.hanja || ''}
+${result.meanings.map(formatMeaning).join('\n')}
+`
+}
+
+bot.on('text', async (ctx: any, next: any) => {
+  const { message } = ctx
+  if (message.entities) {
+    return next()
+  }
+  const text: string = ctx.message.text
+  const results = await search(text.trim().toLowerCase())
+  if (results.length === 0) {
+    return ctx.reply('No results :(')
+  }
+  return ctx.replyWithMarkdown(results.map(formatResult).join('\n'))
+})
+
+export const handleUpdate = bot.handleUpdate

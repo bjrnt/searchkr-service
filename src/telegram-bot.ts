09b8f02d28d7cfd22888b45e7cc87ea17fb0981c
Björn Tegelund <bjorn@tumblbug.com>
2018-01-03 14:22:32 +0900

    Add save button to bot

--- src/telegram-bot.ts
+++ src/telegram-bot.ts
@@ -18,17 +18,35 @@ ${result.meanings.map(formatMeaning).join('\n')}
 `
 }
 
+bot.on('callback_query', (ctx: any, next: any) => {
+  const word: string = ctx.callbackQuery.data
+  // TODO: add actual saving
+  ctx.editMessageReplyMarkup({})
+  ctx.answerCbQuery(`${word} has been saved`)
+})
+
 bot.on('text', async (ctx: any, next: any) => {
   const { message } = ctx
   if (message.entities) {
     return next()
   }
-  const text: string = ctx.message.text
-  const results = await search(text.trim().toLowerCase(), { matchExactly: true })
+  const text: string = ctx.message.text.trim().toLowerCase()
+  const results = await search(text, { matchExactly: true })
   if (results.length === 0) {
     return ctx.reply('No results :(')
   }
-  return ctx.replyWithMarkdown(results.map(formatResult).join('\n'))
+  return ctx.replyWithMarkdown(results.map(formatResult).join('\n'), {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Save',
+            callback_data: text,
+          },
+        ],
+      ],
+    },
+  })
 })
 
 export const handleUpdate = bot.handleUpdate.bind(bot)
